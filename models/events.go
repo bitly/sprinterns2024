518f149936efb85f0fe20e8088909ec2087bfb95
dianaabishop <dianaabishop@users.noreply.github.com>
2024-01-02 10:03:45 -0500

    Update events.go
    Remove RSVP structs

--- models/events.go
+++ models/events.go
@@ -14,17 +14,3 @@ type Event struct {
 	MaxAttendees  int    `json:"max_attendees" `
 	ImageURL      string `json:"image_url"`
 }
-
-type CreateRSVP struct {
-	EventID       int    `json:"event_id" binding:"required"`
-	ResponderName string `json:"name" binding:"required"`
-	RSVP          string `json:"rsvp" binding:"required"`
-}
-
-type GetRSVP struct {
-	ResponseID int    `json:"response_id"`
-	EventID    int    `json:"event_id"`
-	Name       string `json:"name`
-	RSVP       string `json:"rsvp" `
-	CommentID  *int   `json:"comment_id" `
-}
