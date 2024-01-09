ea24cb06209c510216818e0d076162c526495da3
IMightBeAce <vidiyadawah@gmail.com>
2024-01-09 15:03:00 -0500

    added event details and reformatted the page

--- views/js/evently/src/RSVPButton.jsx
+++ views/js/evently/src/RSVPButton.jsx
@@ -1,46 +1,73 @@
-import React from 'react'
-import './RSVPButton.css'
+import React, {useState, useEffect} from 'react';
+import './RSVPButton.css';
     
 function RSVPButton() {
+  const [eventData, setEventDetails]= useState([]);
+   const getEvent = async (e) => {
+    
+   
+    try {
+      let res = await fetch("http://localhost:3000/api/event/5",  {
+        method: "GET",
+        mode: "cors",
+      });
+      if (res.status === 201) {
+        let data = await res.json()
+        setEventDetails(data)
+        
+       }
+     }
+     catch (err){
+       console.log(err);
+     }
+   }
+   useEffect(()=>{
+    getEvent()
+   }, [])
+   console.log(eventData)
+
     return (
      <div className="rsvp-event">
-       
-    
-        {/*<h1>"Event Name: ___"</h1> 
-        <h2> "Location:___"</h2>
-        <h2> "Description:___"</h2>  {/*Is this the same description as the one the user is suppose to enter? It might be too long for the design */}
+        <div className="user-info">
+          <h1 className="event-title">{eventData.event_title}</h1> 
+          <div className="image-container">
+          <img
+            src={eventData.image_url}
+            alt="User inputted description"  
+            style={{
+               width: "100%",
+               height: "100%", 
+             }}
+          />
+           </div>
 
-        <div className="UserInfo">
-          <h1>"Event Name: ___"</h1> 
-          <h2> "Location:___"</h2>
-          <h2> "Description:___"</h2> 
-          <form action="action_page.php">
-            <div className="row">
-              <div className="col-25">
-                <label htmlFor="Email">Email</label>
-              </div>
-              <div className="col-75">
-                <input type="text" id="Email" name="Email" placeholder="Enter your email..." />
-              </div>
-              <br />
-            </div>
-            <div className="row">
-              <div className="col-25">
-                <label htmlFor="phoneNum">Phone Number</label>
-              </div>
-              <div className="col-75">
-                <input type="text" id="phoneNum" name="Phone #" placeholder="Enter your phone number..." />
-              </div>
-              <br />
-            </div>
-          </form>
-          <br />
-          <div className="button">
-            <p>RSVP Here!</p>
+        <div className="event-details">
+          <div className="location-description">
+          <p> 📍 {eventData.location}</p>
+          <p> {eventData.description}</p>
           </div>
-        </div>
 
-     
+          <div className="contact-container">
+          <p> Host Name: {eventData.host_name}</p>
+          <p>Contact: {eventData.contact_info}</p>
+          </div>
+       
+       <div className="date-time-container">
+          <p> Date: {eventData.date}</p>
+          <p> Time: {eventData.time}</p>
+       </div>
+          
+
+        <div className="max-rsvp-container">
+          <p> Max Attendees: {eventData.max_attendees}</p>
+          <p> RSVPs: {eventData.num_of_RSVP}</p>
+          </div>
+       
+        </div >
+          <button className="rsvp-button" onClick={getEvent}>
+            RSVP!
+          </button>
+        </div> 
     </div>
  )
 };
