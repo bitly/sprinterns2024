be98d91e4db1bd371a5dda67555bdcf44d8a843f
Lameya56 <Lameyadia@gmail.com>
2024-01-10 14:25:37 -0500

    updated about_us page

--- views/js/evently/src/AboutUs.jsx
+++ views/js/evently/src/AboutUs.jsx
@@ -8,77 +8,74 @@ import profileImage4 from './profile_img.jpg';
 
 function AboutUs() {
     return (
+        
         <div className="AboutUs">
             <div className= "header">
                 <h1>Our Mission</h1>
-            </div>
-            
+            </div>        
             <div className="missionStatement">
                 <p>Our mission is to curate unforgettable experiences by seamlessly connecting individuals and communities through innovative and diverse events. We strive to inspire and empower event organizers, fostering a platform that celebrates creativity, fosters meaningful connections, and leaves a lasting impact on every participant.</p>
             </div>
-            {/* <div className="profileImages">
-                <img src={profileImage1} alt="Profile 1" />
-                <img src={profileImage2} alt="Profile 2" />
-                <img src={profileImage3} alt="Profile 3" />
-                <img src={profileImage4} alt="Profile 4" />
-            </div> */}
-            
-            <div className="personInfo">
-                {/* Information about each person */}
-                <div className="person">
-                    <div className="profileImages">
-                        <img src={profileImage1} alt="Person 1" />
-                    </div>
-                    <h2>Name 1</h2>
-                    <p>Information about person 1...</p>
-                    <div className="socialMediaLinks">
-                        <a href="#">Social Media 1</a>
-                    </div>    
-                </div>
-                <div className="person">
-                    <div className="profileImages">
-                        <img src={profileImage2} alt="Person 2" />
-                    </div>
-                    <h2>Name 2</h2>
-                    <p>Information about person 2...</p>
-                    <div className="socialMediaLinks">
-                        <a href="#">Social Media 2</a>
-                    </div> 
-                </div>
-                <div className="person">
-                    <div className="profileImages">
-                        <img src={profileImage3} alt="Person 3" />
-                    </div>
-                    <h2>Name 3</h2>
-                    <p>Information about person 3...</p>
-                    <div className="socialMediaLinks">
-                        <a href="#">Social Media 3</a>
-                    </div> 
-                </div>
-                <div className="person">
-                    <div className="profileImages">
-                        <img src={profileImage4} alt="Person 4" />
-                    </div>
-                    <h2>Name 4</h2>
-                    <p>Information about person 4...</p>
-                    <div className="socialMediaLinks">
-                        <a href="#">Social Media 4</a>
-                    </div> 
-                </div>
-                <div className="person">
-                    <div className="profileImages">
-                        <img src={profileImage1} alt="Person 5" />
-                    </div>
-                    <h2>Name 5</h2>
-                    <p>Information about person 5...</p>
-                    <div className="socialMediaLinks">
-                        <a href="#">Social Media 5</a>
-                    </div> 
-                </div>
+            <div className="header">
+                <h2>Meet the Creators!</h2>
             </div>
+           <div className="container-1">
+                <div className="profileImages-1">
+                    <img src={profileImage1} alt="Person 1" />
+                </div>
+                <div className="personInfo-1">
+                    <h2>Safiya Jones</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quas, sapiente, dignissimos eveniet provident non error, beatae dolorum cum doloremque molestias? Repudiandae sint saepe suscipit! Magnam saepe accusantium ullam aliquid labore debitis. Nulla culpa dicta repellat ex temporibus soluta magnam. Nostrum adipisci optio pariatur omnis dolorum voluptate! Tempore, aut. Veritatis?</p>
+                    <p>Connect with me on LinkedIn!</p>
+                    <a href="#">LinkedIn</a>
+                </div>
+           </div>
+           <div className="container-2">
+                <div className="profileImages-2">
+                    <img src={profileImage1} alt="Person 1" />
+                </div>
+                <div className="personInfo-2">
+                    <h2>Lameya Mostafa</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quas, sapiente, dignissimos eveniet provident non error, beatae dolorum cum doloremque molestias? Repudiandae sint saepe suscipit! Magnam saepe accusantium ullam aliquid labore debitis. Nulla culpa dicta repellat ex temporibus soluta magnam.</p>
+                    <p>Let's link up on LinkedIn!</p>
+                    <a href="#">LinkedIn</a>
+                </div>
+           </div>
+           <div className="container-1">
+                <div className="profileImages-1">
+                    <img src={profileImage1} alt="Person 1" />
+                </div>
+                <div className="personInfo-1">
+                    <h2>Sanjida Kazi</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quas, sapiente, dignissimos eveniet provident non error, beatae dolorum cum doloremque molestias? Repudiandae sint saepe suscipit! Magnam saepe accusantium ullam aliquid labore debitis. Nulla culpa dicta repellat ex temporibus soluta magnam. Nostrum adipisci optio pariatur omnis dolorum voluptate! Tempore, aut. Veritatis?</p>
+                    <p>Connect with me on LinkedIn!</p>
+                    <a href="#">LinkedIn</a>
+                </div>
+           </div>
+           <div className="container-2">
+                <div className="profileImages-2">
+                    <img src={profileImage1} alt="Person 1" />
+                </div>
+                <div className="personInfo-2">
+                    <h2>Vidiya Dawah</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quas, sapiente, dignissimos eveniet provident non error, beatae dolorum cum doloremque molestias? Repudiandae sint saepe suscipit! Magnam saepe accusantium ullam aliquid labore debitis. Nulla culpa dicta repellat ex temporibus soluta magnam.</p>
+                    <p>Let's link up on LinkedIn</p>
+                    <a href="#">LinkedIn</a>
+                </div>
+           </div>
+           <div className="container-1">
+                <div className="profileImages-1">
+                    <img src={profileImage1} alt="Person 1" />
+                </div>
+                <div className="personInfo-1">
+                    <h2>Iden Amoako</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quas, sapiente, dignissimos eveniet provident non error, beatae dolorum cum doloremque molestias? Repudiandae sint saepe suscipit! Magnam saepe accusantium ullam aliquid labore debitis. Nulla culpa dicta repellat ex temporibus soluta magnam. Nostrum adipisci optio pariatur omnis dolorum voluptate! Tempore, aut. Veritatis?</p>
+                    <p>Connect with me on LinkedIn!</p>
+                    <a href="#">LinkedIn</a>
+                </div>
+           </div>    
         </div>
-        
-    )
-};
+    ) 
+}; 
 
 export default AboutUs;
