cffb9c334f075da04dfea8ad81c7cab1c3e5732e
IMightBeAce <vidiyadawah@gmail.com>
2024-01-11 10:43:24 -0500

    fixed the indentation

--- views/js/evently/src/RSVPButton.jsx
+++ views/js/evently/src/RSVPButton.jsx
@@ -28,47 +28,47 @@ function RSVPButton() {
 
     return (
      <div className="rsvp-event">
-        <div className="user-info">
-          <h1 className="event-title">{eventData.event_title}</h1> 
-          <div className="image-container">
-          <img
-            src={eventData.image_url}
-            alt="User inputted description"  
-            style={{
-               width: "100%",
-               height: "100%", 
-             }}
-          />
-           </div>
+            <div className="user-info">
+                <h1 className="event-title">{eventData.event_title}</h1> 
+                <div className="image-container">
+                    <img
+                        src={eventData.image_url}
+                        alt="User inputted description"  
+                        style={{
+                          width: "100%",
+                          height: "100%", 
+                        }}
+                    />
+                </div>
 
-        <div className="event-details">
-          <div className="location-description">
-          <p> 📍 {eventData.location}</p>
-          <p> {eventData.description}</p>
-          </div>
+                <div className="event-details">
+                    <div className="location-description">
+                      <p> 📍 {eventData.location}</p>
+                      <p> {eventData.description}</p>
+                    </div>
 
-          <div className="contact-container">
-          <p> Host Name: {eventData.host_name}</p>
-          <p>Contact: {eventData.contact_info}</p>
-          </div>
-       
-       <div className="date-time-container">
-          <p> Date: {eventData.date}</p>
-          <p> Time: {eventData.time}</p>
-       </div>
-          
+                    <div className="contact-container">
+                    <p> Host Name: {eventData.host_name}</p>
+                    <p>Contact: {eventData.contact_info}</p>
+                    </div>
+              
+                    <div className="date-time-container">
+                        <p> Date: {eventData.date}</p>
+                        <p> Time: {eventData.time}</p>
+                    </div>
+                  
 
-        <div className="max-rsvp-container">
-          <p> Max Attendees: {eventData.max_attendees}</p>
-          <p> RSVPs: {eventData.num_of_RSVP}</p>
-          </div>
-       
-        </div >
-          <button className="rsvp-button" onClick={getEvent}>
-            RSVP!
-          </button>
-        </div> 
-    </div>
+                    <div className="max-rsvp-container">
+                      <p> Max Attendees: {eventData.max_attendees}</p>
+                      <p> RSVPs: {eventData.num_of_RSVP}</p>
+                    </div>
+              
+                </div >
+                <button className="rsvp-button" onClick={getEvent}>
+                  RSVP!
+                </button>
+            </div> 
+        </div>
  )
 };
 export default RSVPButton;
