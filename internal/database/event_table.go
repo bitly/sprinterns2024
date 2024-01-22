22778972a368bf0ac15109bf9ff983bee1fea1d8
IMightBeAce <vidiyadawah@gmail.com>
2024-01-22 13:59:17 -0500

    host id randomly generates when creating an event

--- internal/database/event_table.go
+++ internal/database/event_table.go
@@ -5,6 +5,8 @@ import (
 )
 
 func CreateEvent(event models.Event) (*models.Event, error) {
+	// hostrow, err := dbmap.Query(
+	// 	"SELECT host_id, first_name, last_name, email, image_url FROM host WHERE first_name =?", event.HostName)
 	_, err := dbmap.Query(
 		"INSERT INTO event (title, date, time, location, host_name, description, contact_info, public_private, num_of_RSVP, max_attendees, image_url, event_type, host_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);",
 		event.EventTitle, event.Date, event.Time, event.Location, event.HostName, event.Description, event.ContactInfo, event.PublicPrivate, event.NumRSVP, event.MaxAttendees, event.ImageURL, event.EventType, event.HostID)
@@ -52,7 +54,7 @@ func GetEvent(eventID int) (*models.Event, error) {
 	return &events[0], nil
 }
 
-// Creating the RSVP Function 
+// Creating the RSVP Function
 // the rsvp struct created in the models is passed in as a parameter
 // we want to return the data that is in our struct
 func CreateRSVP(rsvp models.RSVP) (*models.RSVP, error) {
@@ -61,40 +63,36 @@ func CreateRSVP(rsvp models.RSVP) (*models.RSVP, error) {
 		"INSERT INTO rsvp (event_id, first_name, last_name, phone_number, email, rsvp_response) VALUES (?,?,?,?,?,?);",
 		rsvp.EventID, rsvp.FirstName, rsvp.LastName, rsvp.PhoneNumber, rsvp.Email, rsvp.Response)
 
+	if err != nil {
+		return nil, err
+	}
+
+	// each row has a column that specifies what type of information needs to be inputed, determined by the DB schema.
+	rsvprow, err := dbmap.Query(
+		"SELECT rsvp_id, event_id, first_name, last_name, phone_number, email, rsvp_response FROM rsvp ORDER BY rsvp_id DESC LIMIT 1")
+	var rsvps []models.RSVP
+
+	// for each row inside of the rsvps array, we are appending an rsvp into it
+	// iterating through each rsvprow and reading the data that is stored in the address, this is then stored inside of the rsvp variable
+	for rsvprow.Next() {
+		var rsvp models.RSVP
+		err = rsvprow.Scan(&rsvp.RSVPID, &rsvp.EventID, &rsvp.FirstName, &rsvp.LastName, &rsvp.PhoneNumber, &rsvp.Email, &rsvp.Response)
 		if err != nil {
 			return nil, err
 		}
 
-		// each row has a column that specifies what type of information needs to be inputed, determined by the DB schema.
-		rsvprow, err := dbmap.Query(
-			"SELECT rsvp_id, event_id, first_name, last_name, phone_number, email, rsvp_response FROM rsvp ORDER BY rsvp_id DESC LIMIT 1")
-		var rsvps []models.RSVP
+		// appending the rsvp into the rsvps array
+		rsvps = append(rsvps, rsvp)
+	}
 
-		// for each row inside of the rsvps array, we are appending an rsvp into it
-		// iterating through each rsvprow and reading the data that is stored in the address, this is then stored inside of the rsvp variable
-		for rsvprow.Next() {
-			var rsvp models.RSVP
-			err = rsvprow.Scan(&rsvp.RSVPID, &rsvp.EventID, &rsvp.FirstName, &rsvp.LastName, &rsvp.PhoneNumber, &rsvp.Email, &rsvp.Response)
-			if err != nil {
-				return nil, err
-			}
-
-			// appending the rsvp into the rsvps array
-			rsvps = append(rsvps, rsvp)
-		}
-
-
-
-		return &rsvps[0], nil
+	return &rsvps[0], nil
 }
 
-
-
 // create a function for getting RSVPS by eventID
-// we are passing in eventID in as a paramter 
+// we are passing in eventID in as a paramter
 // we want an array of the RSVP model to be returned, as well as an error if we encounter one
 func GetRSVPsByEventId(eventID int) ([]models.RSVP, error) {
-	// an RSVPSs array 
+	// an RSVPSs array
 	var rsvps []models.RSVP
 
 	// we want each row inside of the array to have the same eventID, we are getting this from the rsvp table in the DB
@@ -105,16 +103,16 @@ func GetRSVPsByEventId(eventID int) ([]models.RSVP, error) {
 
 	// this for loop is interating through each RSVP row
 	for rsvprow.Next() {
-		var rsvp models.RSVP 
+		var rsvp models.RSVP
 
-		// reading the values in the row and storing it inside of the rsvp variable 
+		// reading the values in the row and storing it inside of the rsvp variable
 		err = rsvprow.Scan(&rsvp.RSVPID, &rsvp.EventID, &rsvp.FirstName, &rsvp.LastName, &rsvp.PhoneNumber, &rsvp.Email, &rsvp.Response)
-			if err != nil {
-				return nil, err
-			}
+		if err != nil {
+			return nil, err
+		}
 
-			// we are adding each rsvp that has been read from the rsvprow inside the rsvps array we initially created.
-			rsvps = append(rsvps, rsvp)
+		// we are adding each rsvp that has been read from the rsvprow inside the rsvps array we initially created.
+		rsvps = append(rsvps, rsvp)
 	}
 
 	// we are returning all of the rsvps, as they will all have the same event ID
@@ -122,53 +120,53 @@ func GetRSVPsByEventId(eventID int) ([]models.RSVP, error) {
 }
 
 func GetAllPublicEvents() ([]models.Event, error) {
-    var publicEvents []models.Event
+	var publicEvents []models.Event
 
-    // Query the database to fetch all public events
-    rows, err := dbmap.Query(
-        "SELECT  event_id, title, date, time, location, host_name, description, contact_info, public_private, num_of_RSVP, max_attendees, image_url, event_type, host_id FROM event WHERE public_private = 'public'")
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+	// Query the database to fetch all public events
+	rows, err := dbmap.Query(
+		"SELECT  event_id, title, date, time, location, host_name, description, contact_info, public_private, num_of_RSVP, max_attendees, image_url, event_type, host_id FROM event WHERE public_private = 'public'")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        var event models.Event
-        err := rows.Scan(
-            &event.EventID, &event.EventTitle, &event.Date, &event.Time,
-            &event.Location, &event.HostName, &event.Description, &event.ContactInfo,
-            &event.PublicPrivate, &event.NumRSVP, &event.MaxAttendees, &event.ImageURL, &event.EventType, &event.HostID)
-        if err != nil {
-            return nil, err
-        }
-        publicEvents = append(publicEvents, event)
-    }
+	for rows.Next() {
+		var event models.Event
+		err := rows.Scan(
+			&event.EventID, &event.EventTitle, &event.Date, &event.Time,
+			&event.Location, &event.HostName, &event.Description, &event.ContactInfo,
+			&event.PublicPrivate, &event.NumRSVP, &event.MaxAttendees, &event.ImageURL, &event.EventType, &event.HostID)
+		if err != nil {
+			return nil, err
+		}
+		publicEvents = append(publicEvents, event)
+	}
 
-    return publicEvents, nil
+	return publicEvents, nil
 }
 
 func GetEventsByField(field, value string) ([]models.Event, error) {
-    var filteredEvents []models.Event
+	var filteredEvents []models.Event
 
-    // Query the database to fetch events based on the specified field and value
-    query := "SELECT event_id, num_of_RSVP, title, date, time, location, host_name, description, contact_info, public_private, max_attendees, image_url, event_type, host_id FROM event WHERE " + field + "=?"
-    rows, err := dbmap.Query(query, value)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+	// Query the database to fetch events based on the specified field and value
+	query := "SELECT event_id, num_of_RSVP, title, date, time, location, host_name, description, contact_info, public_private, max_attendees, image_url, event_type, host_id FROM event WHERE " + field + "=?"
+	rows, err := dbmap.Query(query, value)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-    for rows.Next() {
-        var event models.Event
-        err := rows.Scan(
-            &event.EventID, &event.NumRSVP, &event.EventTitle, &event.Date, &event.Time,
-            &event.Location, &event.HostName, &event.Description, &event.ContactInfo,
-            &event.PublicPrivate, &event.MaxAttendees, &event.ImageURL, &event.EventType, &event.HostID)
-        if err != nil {
-            return nil, err
-        }
-        filteredEvents = append(filteredEvents, event)
-    }
+	for rows.Next() {
+		var event models.Event
+		err := rows.Scan(
+			&event.EventID, &event.NumRSVP, &event.EventTitle, &event.Date, &event.Time,
+			&event.Location, &event.HostName, &event.Description, &event.ContactInfo,
+			&event.PublicPrivate, &event.MaxAttendees, &event.ImageURL, &event.EventType, &event.HostID)
+		if err != nil {
+			return nil, err
+		}
+		filteredEvents = append(filteredEvents, event)
+	}
 
-    return filteredEvents, nil
+	return filteredEvents, nil
 }
