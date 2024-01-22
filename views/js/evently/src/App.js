ffc889b097833abe3bab6441e27df99b94f02851
Iden Amoako <iden@Idens-MacBook-Pro.local>
2024-01-22 15:11:18 -0500

    users are able to update the event. Everything is connected by eventId

--- views/js/evently/src/App.js
+++ views/js/evently/src/App.js
@@ -12,6 +12,7 @@ import CommunityPage from './CommunityPage.jsx';
 import AboutUs from './AboutUs.jsx';
 import RSVPForm from "./RSVPForm.jsx";
 import RSVPButton from "./RSVPButton.jsx";
+import UpdateEventForm from "./UpdateEventForm.jsx";
 
 function App() {
   return (
@@ -23,7 +24,8 @@ function App() {
         <Route path="/create-event" element={<CreateEvent />} />
         <Route path="/RSVP/:eventId" element={<RSVPButton />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="/rsvp-form/:eventTitle" element={<RSVPForm/>} />
+        <Route path="/rsvp-form/:eventId" element={<RSVPForm/>} />
+        <Route path="/update-event/:eventId" element={<UpdateEventForm />} />
       </Routes>
     </div>
   );
